d7f0b6d040c913e51e62d67fcb724099a60aa51c
sitarelli <149289661+sitarelli@users.noreply.github.com>
2026-02-16 22:18:02 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -443,7 +443,7 @@ export function stopAllSounds() {
 }
 
 // Inizializza renderer
-initRenderer(canvas, ctx, sprites, config);
+initRenderer(canvas, ctx, sprites);
 initInput(audioManager.audioContext || new (window.AudioContext || window.webkitAudioContext)());
 
 // Carica un livello
